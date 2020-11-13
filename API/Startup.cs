fd0746ee3e16202fbe430732f3d6a6214eba1ee6
carlCarlson6 <bullet.carlos@gmail.com>
2020-11-13 23:15:42 +0100

    configure DI adn IoC

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,8 +1,13 @@
+using Application.UserUseCases;
+using Domain.Entities;
+using Domain.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using MongoRepository.Settings;
 
 namespace API
 {
@@ -18,7 +23,25 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            this.ConfigureRepositories(services);   
+            this.ConfigureUseCases(services);
+            this.ConfigureDomain(services);
+
             services.AddControllers();
+        }
+
+        public void ConfigureRepositories(IServiceCollection services)
+        {
+            services.Configure<UserMongoSettings>(Configuration.GetSection(nameof(UserMongoSettings)));
+            services.AddSingleton<IMongoRepositorySettings<User>, UserMongoSettings>(serviceProvider => serviceProvider.GetRequiredService<IOptions<UserMongoSettings>>().Value);
+        }
+        public void ConfigureUseCases(IServiceCollection services)
+        {
+            services.AddSingleton<CreateUser>();
+        }
+        public void ConfigureDomain(IServiceCollection services)
+        {
+            services.AddSingleton<UserFinder>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
