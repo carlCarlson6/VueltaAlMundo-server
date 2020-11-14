e3e909da95fbf42ff8c0c1d1d8b4dd22cef266d9
carlCarlson6 <bullet.carlos@gmail.com>
2020-11-14 11:51:23 +0100

    changed method name

--- Application/UserUseCases/ListAllUsers.cs
+++ Application/UserUseCases/ListAllUsers.cs
@@ -1,13 +1,17 @@
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Domain.Repositories;
 
 namespace Application.UserUseCases
 {
     public class ListAllUsers
     {
         private readonly IUserRepository repostory;
-        public ListAllUsers(IUserRepository userRepostory) => this.repostory = userRepository;
+        public ListAllUsers(IUserRepository userRepostory) => this.repostory = userRepostory;
 
-        public async Task<List<User>> ListAll() => await this.repostory.Read();
+        public async Task<List<User>> Execute() => await this.repostory.Read();
         
     }
 }
