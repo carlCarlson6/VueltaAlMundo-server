168b25e2472abf127491d6d98ae8470111482e07
carlCarlson6 <bullet.carlos@gmail.com>
2020-11-15 19:43:57 +0100

    misc

--- Domain/Repositories/IUserModel.cs
+++ Domain/Repositories/IUserModel.cs
@@ -4,9 +4,9 @@ namespace Domain.Repositories
 {
     public interface IUserModel
     {
-        String id {get; set;}
-        String email {get; set;}
-        String name {get; set;}
-        String password {get; set;}
+        String id { get; set; }
+        String email { get; set; }
+        String name { get; set; }
+        String password { get; set; }
     }
 }
