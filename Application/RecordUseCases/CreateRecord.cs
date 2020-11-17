d3d459ea3c313058989d64fd93078d7e2be7053c
carlCarlson6 <bullet.carlos@gmail.com>
2020-11-17 23:14:25 +0100

    check if user exists before creating the record

--- Application/RecordUseCases/CreateRecord.cs
+++ Application/RecordUseCases/CreateRecord.cs
@@ -2,19 +2,24 @@ using System.Threading.Tasks;
 using Application.RecordDTOs;
 using Domain.Entities;
 using Domain.Repositories;
+using Domain.Services;
+using Domain.ValueObjects;
 
 namespace Application.RecordUseCases
 {
     public class CreateRecord
     {
         private readonly IRecordRepository repository;
-        public CreateRecord(IRecordRepository recordRepository)
+        private readonly UserFinder finder;
+        public CreateRecord(IRecordRepository recordRepository, UserFinder userFinder)
         {
             this.repository = recordRepository;
+            this.finder = userFinder;
         }
 
         public async Task<Record> Execute(CreateRecordCommand command)
         {
+            await this.finder.Find(new UserId(command.UserId));
             Record newRecord = Record.Create(command.UserId, command.Kilometers, command.CreatedAt);
             await this.repository.Save(newRecord);
 
