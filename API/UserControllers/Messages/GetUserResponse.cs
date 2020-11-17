ae00f963e10b19bf023a65aacf24195d629a12fb
carlCarlson6 <bullet.carlos@gmail.com>
2020-11-17 23:06:35 +0100

    deleted email from response

--- API/UserControllers/Messages/GetUserResponse.cs
+++ API/UserControllers/Messages/GetUserResponse.cs
@@ -6,13 +6,11 @@ namespace API.UserControllers.Messages
     public class GetUserResponse
     {
         public String Id { get; set; }
-        public String Email { get; set; }
         public String Name { get; set; }
 
         public GetUserResponse(User user)
         {
             this.Id = user.Id.Value;
-            this.Email = user.Email.Value;
             this.Name = user.Name.Value;
         }
     }
