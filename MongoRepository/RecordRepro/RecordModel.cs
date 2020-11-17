7a327e7a662353b3fd77b0fc5a12b4703568e21d
carlCarlson6 <bullet.carlos@gmail.com>
2020-11-17 23:51:06 +0100

    added constructor

--- MongoRepository/RecordRepro/RecordModel.cs
+++ MongoRepository/RecordRepro/RecordModel.cs
@@ -1,4 +1,5 @@
 using System;
+using Domain.Entities;
 using Domain.Repositories;
 
 namespace MongoRepository.RecordRepro
@@ -8,7 +9,16 @@ namespace MongoRepository.RecordRepro
         public String id { get; set; }
         public String userId { get; set; }
         public Double kilometers { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime StoredAt { get; set; }
+        public DateTime createdAt { get; set; }
+        public DateTime storedAt { get; set; }
+
+        public RecordModel(Record record)
+        {
+            this.id = record.Id.Value;
+            this.userId = record.UserId.Value;
+            this.kilometers = record.Kilometers.Value;
+            this.createdAt = record.CreatedAt;
+            this.storedAt = record.StoredAt;
+        }
     }
 }
